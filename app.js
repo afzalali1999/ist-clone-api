4aec6d77a940b8c5f056a1f254c3e637943e88ac
Afzal Ali <afzalali1999@gmail.com>
2020-12-17 11:50:12 -0500

    added rest of the api data

--- app.js
+++ app.js
@@ -18,6 +18,54 @@ app.get('/api/about', (req, res) => {
     })
 });
 
+app.get('/api/degrees', (req, res) => {
+    fs.readFile('./data/degrees.json', 'utf8', (error, data) => {
+        if (error) throw error;
+
+        res.send(JSON.parse(data));
+    })
+});
+
+app.get('/api/minors', (req, res) => {
+    fs.readFile('./data/minors.json', 'utf8', (error, data) => {
+        if (error) throw error;
+
+        res.send(JSON.parse(data));
+    })
+});
+
+app.get('/api/employers', (req, res) => {
+    fs.readFile('./data/employers.json', 'utf8', (error, data) => {
+        if (error) throw error;
+
+        res.send(JSON.parse(data));
+    })
+});
+
+app.get('/api/people', (req, res) => {
+    fs.readFile('./data/people.json', 'utf8', (error, data) => {
+        if (error) throw error;
+
+        res.send(JSON.parse(data));
+    })
+});
+
+app.get('/api/research', (req, res) => {
+    fs.readFile('./data/research.json', 'utf8', (error, data) => {
+        if (error) throw error;
+
+        res.send(JSON.parse(data));
+    })
+});
+
+app.get('/api/footer', (req, res) => {
+    fs.readFile('./data/footer.json', 'utf8', (error, data) => {
+        if (error) throw error;
+
+        res.send(JSON.parse(data));
+    })
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
